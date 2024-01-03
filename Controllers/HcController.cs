c0578455faab86695d05e46c88c663694706a340
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2024-01-03 20:59:38 +0100

    configured postgres, local appsettings.json

---
+++ Controllers/HcController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hc_backend.Controllers
+{
+    public class HcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Provider.cs
@@ -0,0 +1,8 @@
+﻿namespace hc_backend.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
