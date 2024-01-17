8918fbb09cd7f7bc87bd6d5029b187ced9183cfd
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-17 20:39:31 +0100

    Create Feedback model

---
+++ Models/Feedback.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hc_backend.Models
+{
+    public class Feedback
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+        // Patient not required, if a patient wants to leave anonymous feedback.
+        public int? PatientId { get; set; }
+        public Patient? Patient { get; set; }
+    }
+}
