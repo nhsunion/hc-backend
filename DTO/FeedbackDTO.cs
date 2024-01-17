f36a900108ac0b557548926ec918e2887c51ee66
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-17 21:41:53 +0100

    Create Feedback DTO

---
+++ DTO/FeedbackDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hc_backend.DTO
+{
+    public class FeedbackDTO
+    {
+        [Required]
+        public string Message { get; set; }
+
+        public int? PatientId { get; set; }
+    }
+}
