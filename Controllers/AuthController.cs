e5e31ff7f8a4e07e86d123947fa084ad088eed88
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-17 13:32:46 +0100

    Surround GenerateTokenProvider in try-catch

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -117,7 +117,17 @@ namespace hc_backend.Controllers
                 return BadRequest("Incorrect Username or Password");
             }
 
-            var token = _authService.GenerateTokenProvider(provider);
+            string? token = null;
+
+            try
+            {
+                token = _authService.GenerateTokenProvider(provider);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception during token generation: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
 
             Response.Cookies.Append("jwt", token, new CookieOptions
             {
