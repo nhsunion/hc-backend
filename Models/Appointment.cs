0ffc1041f3aa88ec5c6df6a26a686c0bfb2a8317
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-23 22:22:13 +0100

    Update Appointment.cs

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -1,12 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 using hc_backend.Models;
 
-public class Appointment {
+public class Appointment
+{
+    [Required]
     public int Id { get; set; }
     [Required]
     public DateTime Date { get; set; }
     public int? PatientId { get; set; }
-    public Patient Patient { get; set; }
+    public Patient ?Patient { get; set; }
     [Required]
     public int ProviderId { get; set; }
     public Provider Provider { get; set; }
