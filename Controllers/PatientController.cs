60603fd194c25c743d0f931c77b73d56e2a7e64e
TimNilssoon <tim.nilssoon@outlook.com>
2024-01-25 12:59:41 +0100

    Implement PatientController

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,39 @@
+﻿using hc_backend.Data;
+using hc_backend.DTO;
+using hc_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace hc_backend.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly AppDbcontext _db;
+        public PatientController(AppDbcontext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("patients")]
+        public async Task<ActionResult<IEnumerable<PatientDTO>>> GetAllPatients()
+        {
+            var patients = await _db.Patients.ToListAsync();
+
+            List<PatientDTO> patientDTOS = new();
+            foreach (var patient in patients)
+            {
+                patientDTOS.Add(new PatientDTO
+                {
+                    Id = patient.Id,
+                    Name = patient.Name,
+                    Email = patient.Email,
+                    Phone = patient.Phone,
+                    Address = patient.Address,
+                    Username = patient.Username
+                });
+            }
+
+            return patientDTOS;
+        }
+    }
+}
